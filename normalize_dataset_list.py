974e1b315de7917948d9e0ef27fba86910a1fbd9
Colin Shea <cshea3@umbc.edu>
2017-05-04 23:14:17 -0400

    updated normalized_dataset_list to thread correct

--- normalize_dataset_list.py
+++ normalize_dataset_list.py
@@ -6,8 +6,60 @@ from sys import executable
 import subprocess
 from subprocess import Popen, PIPE
 import argparse
-
+from multiprocessing import Process
+import json 
+import numpy as np
+import glob
 #https://www.ibm.com/developerworks/aix/library/au-multiprocessing/index.html
+
+def parse_files(path_set,destination_dir,list_of_files,data_set_features_values):
+    print("IN the parse_file")
+    for items in list_of_files:
+        print(path_set+items)
+        for filename in glob.iglob(path_set+items+'/*.json',recursive=True):
+            print(filename)
+            with open(filename) as data_file:
+                data_to_normalize = json.load(data_file)
+                
+                # include only Zeus, Crypto, Locker, and APT1 
+                # and ignore other malware families
+                lbl = data_to_normalize["properties"]["label"]
+                if not lbl == "APT1" and \
+                  not lbl == "Crypto" and \
+                  not lbl == "Locker" and \
+                  not lbl == "Zeus":
+                    print("SKIPPING: " + filename)
+                    continue
+
+            print("NORMALIZING: " + filename)
+            
+            temp_normalized_dict = {}
+            properties = sorted((data_to_normalize["properties"]).keys())
+            for feature in properties:
+                # get vector length of this feature
+                vector_len = len(data_set_features_values[feature])
+
+                # initialize to all zeros  
+                temp_normalized_dict[feature] = list(map(lambda x: 0, range(vector_len)))
+    
+                # set indices to 0 or 1
+                feature_values_str = data_to_normalize["properties"][feature]
+                feature_values_list = [x for x in feature_values_str.split()]
+                for f_value in feature_values_list:
+                    # get index from dataset
+                    idx = data_set_features_values[feature].index(f_value)
+
+                    #print("feature: " + feature + " : " + "value " + f_value + " idx: " + str(idx))
+
+                    # set index to "1" since this feature-value is present
+                    temp_normalized_dict[feature][idx] = 1
+            
+            # output normalized sample to file in new directory
+            normalized_filename =  os.path.splitext(os.path.basename(filename))[0] + "__normalized" + ".json"
+            normalized_filepath = os.path.join(destination_dir, normalized_filename)
+            with open(normalized_filepath, 'w') as outfile:
+                outfile.write(json.dumps(temp_normalized_dict, sort_keys=True, indent=4))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("path_to_dataset", help="path to dataset")
@@ -26,6 +78,12 @@ if __name__ == '__main__':
         shutil.rmtree(args.destination_dir)
     os.mkdir(args.destination_dir)
     
+    with open(args.all_possible_feature_value_filename) as data_file:
+        data_set_features_values = json.load(data_file)
+        for feature in data_set_features_values:
+            print(feature + ": " + str(len(data_set_features_values[feature])))
+    print("--------------------------------------------------")
+
     onlyFolders = [f for f in listdir(args.path_to_dataset)]
     number_of_folders = int(len(onlyFolders))
     folders_per_thread = number_of_folders/(int(args.number_of_threads))
@@ -36,23 +94,33 @@ if __name__ == '__main__':
         remainder = number_of_folders%threads
     
     #[[] for dummy in xrange(int(args.number_of_threads))]
-    work_list = [onlyFolders[i:i+folders_per_thread] for i in range(0, len(onlyFolders), folders_per_thread)]
-    for item in work_list:
-        #cpid = os.fork()
-        #if not cpid:
-        #    import somescript
-        #    os._exit(0)
-        
-    #os.waitpid(cpid, 0)
-        print("call before the Popen")
-        process=subprocess.Popen(['python /data/cwshea/ml_cmsc_678/normalize_dataset_input.py ']+item, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-	stdout,stderr = process.communicate()
-        return_code = process.poll()
-        print("stdout='{}'\nstderr='{}'\nreturn_code='{}'".format(stdout, stderr, return_code))
-        print("call after Popen")
-        #p.communicate()
-    
+    work_list = [onlyFolders[i:i+int(folders_per_thread)] for i in range(0, len(onlyFolders), int(folders_per_thread))]
+    p=np.array([])
+    print(len(work_list))
+    print(len(p))
+    print(type(p))
+    for index,elem in enumerate(work_list):     
+        p=np.append(p,Process(target=parse_files, args=(args.path_to_dataset,args.destination_dir,elem,data_set_features_values)))
+        print(p)
+        p[int(index)].start()
+        #print(index)
+        #print(elem)
+    for index2,elem2 in enumerate(work_list):
+        p[index2].join()
+        print(index2)
     #print work_list
     #print(len(work_list))
-    p_status = p.wait()
-    
+    #cpid = os.fork()
+    #if not cpid:
+    #    import somescript
+    #    os._exit(0)
+        
+    #os.waitpid(cpid, 0)
+    #print("call before the Popen")
+    #process=subprocess.Popen(['python /data/cwshea/ml_cmsc_678/normalize_dataset_input.py ']+item, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    #stdout,stderr = process.communicate()
+    #return_code = process.poll()
+    #print("stdout='{}'\nstderr='{}'\nreturn_code='{}'".format(stdout, stderr, return_code))
+    #print("call after Popen")
+    #p.communicate()
+    #parse_files(path_set,list_of_files)
